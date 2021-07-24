766339c0288ef262fdf487709b6ee1cebfb2030e
Luke Lubbe <luke@nightsbridge.com>
2021-07-24 16:28:28 +0200

    added local captcha validation

--- guesthouseadd.js
+++ guesthouseadd.js
@@ -1,14 +1,26 @@
-$("form").submit(function(e){
-    var form = $(this);
-    $.ajax({
-        url   : form.attr('action'),
-        type  : form.attr('method'),
-        data  : form.serialize(),
+$("form").submit(function (e) {
 
-        //todo fix this up
-        success: function(response){
-            alert(response);
-        }
-    });
-    return false;
+    var response = grecaptcha.getResponse();
+
+    if (response.length == 0) {
+        alert("Please complete the captcha.");
+    }
+    else {
+
+
+    }
+
+    // var form = $(this);
+    // $.ajax({
+    //     url: form.attr('action'),
+    //     type: form.attr('method'),
+    //     data: form.serialize(),
+    //
+    //     //todo fix this up
+    //     success: function (response) {
+    //         alert(response);
+    //     }
+    // });
+
 });
+
